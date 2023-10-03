4803a4b5f2ff01afa0218b5e921daa0dc10f167e
LevonAr <levonrm10@gmail.com>
2023-10-03 15:34:22 -0700

    feat: code before refactor

--- js/main.js
+++ js/main.js
@@ -18,12 +18,37 @@
  * Also feel free to modify this code or the HTML as much as needed.  You do not need to use this structure.
  */
 
-function generateOutput(input) {
+const getMorseCode = async (letter) => {
+    const { data } = await axios.get(`https://safetybelt.pythonanywhere.com/letters/${letter}`);
+    console.log('data', data);
+    return data;
+};
+
+const generateOutput = async (input) => {
     const output = document.getElementById('result-content');
 
-    // translate here
+    console.log('input', input)
+    const inputArray = Array.from(input);
+    console.log('inputArray', inputArray)
 
-    output.innerHTML = input;
+    const morseArray = [];
+    for (let i = 0; i < inputArray.length; i++) {
+        const item = inputArray[i];
+        const res = getMorseCode(item).then((data) => {
+            result = data?.code;
+            return result;
+        });
+        console.log('res', res)
+        morseArray.push(res);
+    }
+
+    const test = await Promise.all(morseArray)
+
+    console.log('morseArray', morseArray)
+    console.log('test', test)
+
+
+    output.innerHTML = test.join(" ") || "";
 }
 
 document.getElementById('text-input').addEventListener('submit', function(e) {
