faf608f0ebf758841d2a44c3d13a67bdbfab49d9
LevonAr <levonrm10@gmail.com>
2023-10-03 15:52:19 -0700

    refactor: minor changes

--- js/main.js
+++ js/main.js
@@ -1,58 +1,28 @@
-/**
- * Morse Code Translator
- * 
- * Use the Letters API built for this interview:
- * https://safetybelt.pythonanywhere.com/letters/<LETTER>
- * 
- *  For example, a GET call to https://safetybelt.pythonanywhere.com/letters/s will return:
- *      {
- *          "code": "..."
- *      }
- * 
- * The API only handles one letter at a time
- *
- * Letters should be broken up with a space.
- * Feel free to use any process for differentiating the space between letters and words
- *     I recommend something like a / or | between words; ie 'dog cat' => '-.. --- --. / -.- .- -'
- *
- * Also feel free to modify this code or the HTML as much as needed.  You do not need to use this structure.
- */
-
 const getMorseCode = async (letter) => {
     const { data } = await axios.get(`https://safetybelt.pythonanywhere.com/letters/${letter}`);
-    console.log('data', data);
     return data;
 };
 
 const generateOutput = async (input) => {
     const output = document.getElementById('result-content');
 
-    console.log('input', input)
     const inputArray = Array.from(input);
-    console.log('inputArray', inputArray)
 
     const morseArray = [];
     for (let i = 0; i < inputArray.length; i++) {
-        const item = inputArray[i];
-        const res = getMorseCode(item).then((data) => {
-            result = data?.code;
-            return result;
-        });
-        console.log('res', res)
-        morseArray.push(res);
-    }
+        const letter = inputArray[i];
+        const morseLetter = getMorseCode(letter).then((data) => (data?.code));
+        morseArray.push(morseLetter);
+    };
+    const result = await Promise.all(morseArray).join('') || '';
 
-    const test = await Promise.all(morseArray)
-
-    console.log('morseArray', morseArray)
-    console.log('test', test)
-
-
-    output.innerHTML = test.join(" ") || "";
+    output.innerHTML = result;
 }
 
-document.getElementById('text-input').addEventListener('submit', function(e) {
-    e.preventDefault();
+document.getElementById('text-input').addEventListener('submit', (e) => {
     const input = document.getElementById('text-to-convert');
+
+    e.preventDefault();
+
     generateOutput(input.value);
 });
